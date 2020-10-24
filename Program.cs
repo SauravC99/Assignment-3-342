69daa97da2768befbb80616c70e9855b4c5a9ff1
Fabian Nunez <Seakr1948@hotmail.com>
2020-10-23 21:06:38 -0700

    Main file with the functions that we need

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Program
+{
+	public Program()
+	{
+		static IEnumerable<string> EnumerateFilesRecursively(string path)
+		{
+
+		}
+
+		static string FormatByteSize(long byteSize)
+		{
+
+		}
+
+		static XDocument CreateReport(IEnumerable<string> files)
+		{
+
+		}
+
+		public static void Main(string[] args)
+		{
+
+		}
+
+	}
+}
