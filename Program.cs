5561e2731249b7eed7c592766fbbd1d0a86ba914
Saurav <38475004+SauravC99@users.noreply.github.com>
2020-10-27 00:45:43 -0700

    Added and implemented FormatByteSize method

--- Program.cs
+++ Program.cs
@@ -16,7 +16,21 @@ public class Program {
 	}
 
 	static string FormatByteSize(long byteSize) {
+		// String list of ends to the size
+		string[] ends = { "Bytes", "kB", "mB", "gB", "tB", "pB" };
+		int counter = 0;
+		// Used float instead of Decimal here cause method only returns 2 siginificant digits
+		// (Decimal has a higher precision than float)
+		float number = (float)byteSize;
 
+		// Loop keep dividing by 1000 until you can't
+		// counter keeps track of which end we are at
+		while (number / 1000 >= 1) {
+			number = number / 1000;
+			counter++;
+		}
+		// Put it together and return, the 0:n2 part will give 2 significant digits
+		return string.Format("{0:n2}{1}", number, ends[counter]);
 	}
 
 	static XDocument CreateReport(IEnumerable<string> files) {
