c5f26212159c97fd5b568fc107e0b8e308eed80a
Saurav <38475004+SauravC99@users.noreply.github.com>
2020-10-27 00:29:42 -0700

    Created a VS C# project and added everything in

--- Program.cs
+++ Program.cs
@@ -1,33 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Security.Cryptography.X509Certificates;
+using System.IO;
+using System.Xml.Linq;
 
 /// <summary>
 /// Summary description for Class1
 /// </summary>
-public class Program
-{
-	public Program()
-	{
-		static IEnumerable<string> EnumerateFilesRecursively(string path)
-		{
-
-		}
-
-		static string FormatByteSize(long byteSize)
-		{
-
-		}
-
-		static XDocument CreateReport(IEnumerable<string> files)
-		{
-
-		}
-
-		public static void Main(string[] args)
-		{
-
-		}
+public class Program {
+	static IEnumerable<string> EnumerateFilesRecursively(string path) {
 
 	}
+
+	static string FormatByteSize(long byteSize) {
+
+	}
+
+	static XDocument CreateReport(IEnumerable<string> files) {
+
+	}
+
+	public static void Main(string[] args) {
+
+	}
+
 }
